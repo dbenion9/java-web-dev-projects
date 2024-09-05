0defbddc910367ce372c242e91ec33c9ce8d2712
DeMarcus Benion <dbenion9@gmail.com>
2024-09-05 08:33:57 -0500

    Data Types

---
+++ datatypes/datatypes-exercises/src/main/java/IO.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Miles {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many miles have you driven?");
+        Double numMiles = input.nextDouble();
+
+        System.out.println("How much gas did you use? In gallons.");
+        Double numGallons = input.nextDouble();
+
+        Double mpg = numMiles / numGallons;
+        System.out.println("You are running on " + mpg + " mpg.");
+    }
+}
+
+Integer index = firstSentence.indexOf(searchTerm);
+Integer length = searchTerm.length();
+   System.out.println("Your search term first appears at index " + index + ". Your term is " + length + " characters long.");
+String modifiedSentence = firstSentence.replace(searchTerm, "");
+   System.out.println(modifiedSentence);
